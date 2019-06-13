24bbb773f479c530eab5e2919f872618ae05aaae
Henrique Breim <henrique@breim.com.br>
2019-06-13 18:45:41 -0300

    New options to return at same key in bucket

--- handler.js
+++ handler.js
@@ -16,24 +16,25 @@ module.exports.convert = async ({ Records: records }, context) => {
               const bucket_name = record.s3['bucket']['name'];
               const video_name = key.split('/').slice(-1)[0];
               const video_base_name = video_name.split('.')[0];
-              
+              const numero_aleatorio = Math.floor(Math.random() * 1000);
+  
               const video = await s3.getObject({
                 Bucket: bucket_name,
                 Key: key
               }).promise();
-                
+
               const inputStream = fs.createWriteStream(`/tmp/${video_name}`);
               inputStream.write(video.Body);
             
-              const { stdout, stderr } = await exec(`/opt/ffmpeg/ffmpeg -i /tmp/${video_name} -vcodec libx264 -pix_fmt yuv420p -profile:v baseline -level 3 -f mp4 /tmp/${video_base_name}.mp4`);
+              const { stdout, stderr } = await exec(`/opt/ffmpeg/ffmpeg -i /tmp/${video_name} -vcodec libx264 -pix_fmt yuv420p -profile:v baseline -level 3 -f mp4 /tmp/${video_base_name}_${numero_aleatorio}.mp4`);
 
-              const outputFileBuffer = await fs.readFileSync(`/tmp/${video_base_name}.mp4`);
-
+              const outputFileBuffer = await fs.readFileSync(`/tmp/${video_base_name}_${numero_aleatorio}.mp4`);
+  
               await s3.putObject({
                 Body: outputFileBuffer,
                 Bucket: bucket_name,
                 ContentType: 'binary/octet-stream',
-                Key: `conversions/${video_base_name}.mp4`
+                Key: key
               }).promise();
 
           } catch(error) {
